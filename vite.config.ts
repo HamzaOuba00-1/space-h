9759e066b21392bbf1321afdee49ba690446c2e7
HamzaOuba00-1 <hamza.ouba.2004@gmail.com>
2026-02-23 19:35:33 +0100

    chore: bootstrap Vite + React + TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
+  },
+
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
